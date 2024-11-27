0411905bc01d9f326ffa169e456ad2dfed35a203
polskipietra <109820324+polskipietra@users.noreply.github.com>
2024-11-27 22:44:11 +0100

    Add files via upload

--- zadanie/script.js
+++ zadanie/script.js
@@ -1,24 +1,41 @@
 const img = document.querySelector("img");
-const button = document.querySelector("button");
+const button = document.querySelector(".generate");
+const buttonLeft = document.querySelector(".left");
+const buttonRight = document.querySelector(".right");
 
-let Date;
-const camera = "opportunity";
-const page = 1;
+let photos = [];
+let currentIndex = 0;
+
 const ApiKey = "CzmpXVn0vTSXgeciOoLiyKkWj4hmfhLkTnTBVylY";
-
-// const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?&camera=${camera}&api_key=CzmpXVn0vTSXgeciOoLiyKkWj4hmfhLkTnTBVylY`;
-// const URL = `https://api.nasa.gov/mars-photos/api/v1/mainfest/Curiosity/api_key=CzmpXVn0vTSXgeciOoLiyKkWj4hmfhLkTnTBVylY`;
 
 button.addEventListener("click", () => {
 	const InputRover = document.querySelector("#rovers");
 	const sol = document.querySelector("#sol");
-	const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${InputRover.value}/photos?sol=${sol.value}&api_key=CzmpXVn0vTSXgeciOoLiyKkWj4hmfhLkTnTBVylY`;
-	console.log(InputRover.value);
-	console.log(sol.value);
+	const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${InputRover.value}/photos?sol=${sol.value}&page=1&api_key=${ApiKey}`;
+
 	fetch(URL)
 		.then((res) => res.json())
-		.then((data) => img.setAttribute("src", data.photos[0].img_src))
-		// img.setAttribute("src", data.photos[1].img_src
+		.then((data) => {
+			photos = data.photos;
+			console.log(photos);
+			if (photos.length > 0) {
+				currentIndex = 0;
+				img.setAttribute("src", photos[currentIndex].img_src);
+			} else {
+				alert("Brak zdjęć dla podanych danych.");
+				img.removeAttribute("src");
+			}
+		})
 		.catch((error) => console.error("Error:", error));
-	console.log("gotowe");
+});
+
+buttonLeft.addEventListener("click", () => {
+	if (photos.length === 0) return;
+	currentIndex = (currentIndex - 1) % photos.length;
+	img.setAttribute("src", photos[currentIndex].img_src);
+});
+buttonRight.addEventListener("click", () => {
+	if (photos.length === 0) return;
+	currentIndex = (currentIndex + 1) % photos.length;
+	img.setAttribute("src", photos[currentIndex].img_src);
 });
